9f39bec9052cb965c29f57514b479a96c5160401
Josh Miner <joshteng@me.com>
2017-07-10 03:44:35 +0800

    copy mine file

--- first_set_up.sh
+++ first_set_up.sh
@@ -13,10 +13,20 @@ mkdir /home/digdug/mining_logs
 touch /home/digdug/mining_logs/mining.log
 touch /home/digdug/mining_logs/raw_mining.log
 
+# Ensure Monit is installed
 # Modify monit startup script
 # /etc/monit/monitrc
 
+# Ensure Logentries is installed
 # Modify logentries log file
 # sudo su -
 # /etc/le/config
+
+# Run nvidia initializer
+
+# Modify Visudo to allow reboot and nvidia-smi without passwords
+
+# Copy mine file
+cp /home/digdug/miner/mine.sh.sample /home/digdug/miner/mine.sh
+echo "Done! Don't forget to change your wallet address"
 
