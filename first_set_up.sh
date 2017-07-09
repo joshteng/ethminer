928540250f6c8f1cba1d1acc8529074820631b46
Josh Miner <joshteng@me.com>
2017-07-10 02:41:17 +0800

    set up script

---
+++ first_set_up.sh
@@ -0,0 +1,15 @@
+# Copy necessary files
+ln -s /home/digdug/ethminer/miner ~/
+ln -s /home/digdug/ethminer/overclock ~/
+ln -s /home/digdug/ethminer/.screenrc ~/
+
+# Make necessary directories
+mkdir /home/digdug/mining_logs
+touch /home/digdug/mining_logs/mining.log
+touch /home/digdug/mining_logs/raw_mining.log
+
+# Modify monit startup script
+
+# Modify logentries log file
+# sudo su -
+
