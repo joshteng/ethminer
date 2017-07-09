8bcaa019f5d6ab386e51208b805150f317e89d4b
Josh Miner <joshteng@me.com>
2017-07-10 03:01:48 +0800

    use central bashrc

--- first_set_up.sh
+++ first_set_up.sh
@@ -1,7 +1,12 @@
 # Copy necessary files
+rm ~/miner
 ln -s /home/digdug/ethminer/miner ~/
+rm ~/overclock
 ln -s /home/digdug/ethminer/overclock ~/
+rm ~/.screenrc
 ln -s /home/digdug/ethminer/.screenrc ~/
+rm ~/.bashrc
+ln -s /home/digdug/ethminer/.bashrc ~/
 
 # Make necessary directories
 mkdir /home/digdug/mining_logs
@@ -9,7 +14,9 @@ touch /home/digdug/mining_logs/mining.log
 touch /home/digdug/mining_logs/raw_mining.log
 
 # Modify monit startup script
+# /etc/monit/monitrc
 
 # Modify logentries log file
 # sudo su -
+# /etc/le/config
 
